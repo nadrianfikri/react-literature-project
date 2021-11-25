7d588fc6b7b2191b59e9235d2982da42923a51f0
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-11-26 00:19:34 +0700

    fix style button

--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -1,6 +1,6 @@
 export default function Button(props) {
   return (
-    <button className={` py-2 px-4 rounded-md ${props.className}`} type={props.type} onClick={props.onClick}>
+    <button className={` p-2 rounded-md ${props.className}`} type={props.type} onClick={props.onClick}>
       {props.text}
     </button>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <Hero />
+      <Hero onSignUp={'registmodal'} onSignIn={'loginmodal'} />
     </div>
   );
 }
