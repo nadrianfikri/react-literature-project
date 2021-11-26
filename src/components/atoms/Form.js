d97d48f310b64c07ac3994a1f1296244b15a1dc6
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-11-26 10:39:53 +0700

    done regist modal

--- src/components/atoms/Form.js
+++ src/components/atoms/Form.js
@@ -3,7 +3,7 @@ function Input(props) {
     <>
       <input
         onChange={props.onChange}
-        className={`p-2 w-full bg-input text-white rounded-md focus:outline-none border-2 border-gray-500 ${props.className}`}
+        className={`p-2 w-full bg-input text-gray-300 rounded-md focus:outline-none border-2 border-gray-500 placeholder-gray-300 ${props.className}`}
         type={props.type}
         placeholder={props.placeholder}
         name={props.name}
@@ -11,6 +11,43 @@ function Input(props) {
         required
       />
     </>
+  );
+}
+
+function Select(props) {
+  return (
+    <div>
+      <label htmlFor={props.labelFor} className="font-bold text-lg">
+        {props.labelName}
+      </label>
+      <select defaultValue={'DEFAULT'} onChange={props.onChange} id={props.id} name={props.name} className={`p-2 w-full bg-input text-gray-300 rounded-md focus:outline-none border-2 border-gray-500 ${props.className}`}>
+        {props.children}
+      </select>
+    </div>
+  );
+}
+
+function Option(props) {
+  return (
+    <>
+      <option onChange={props.onChange} value={props.value} id={props.id} className="bg-primary">
+        {props.field}
+      </option>
+    </>
+  );
+}
+
+function InputImage(props) {
+  return (
+    <div className="form-group space-y-2 pb-20">
+      <label htmlFor={props.labelFor} className="font-bold text-lg">
+        {props.labelName}
+      </label>
+      <label htmlFor={props.labelFor} className="w-max text-yellow-500 bg-gray-200 rounded-md text-lg flex gap-10 cursor-pointer p-2 border-2 border-gray-300">
+        Attach Here <img src="/assets/icons/attach.svg" alt="icon" />
+      </label>
+      <input onChange={props.onChange} type="file" hidden id="images" name="images" multiple />
+    </div>
   );
 }
 
@@ -25,4 +62,4 @@ function DirectLink(props) {
   );
 }
 
-export { Input, DirectLink };
+export { Input, DirectLink, Select, Option };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,18 +3,24 @@ import { useState } from 'react';
 import Header from '../components/organism/Header';
 import Hero from '../components/organism/Hero';
 import Login from './auth/Login';
-import { Input } from '../components/atoms/Form';
+import Register from './auth/Register';
+import { Input, Option, Select } from '../components/atoms/Form';
 
 export default function Home() {
-  const [show, setShow] = useState(false);
+  const [openLgn, setOpenLgn] = useState(false);
+  const [openRgs, setOpenRgs] = useState(false);
 
   // store data with useState
   const [form, setForm] = useState({
     email: '',
     password: '',
+    fullname: '',
+    gender: '',
+    phone: '',
+    address: '',
   });
 
-  const { email, password } = form;
+  const { email, password, fullname, gender, phone, address } = form;
 
   const handleChange = (e) => {
     setForm({
@@ -22,14 +28,39 @@ export default function Home() {
       [e.target.name]: e.target.value,
     });
   };
-  console.log(form);
+  const handleLogin = () => {
+    console.log('login');
+  };
+  const handleRegister = () => {
+    console.log('register');
+  };
+
+  const openAnotherModal = () => {
+    setOpenRgs(!openRgs);
+    setOpenLgn(!openLgn);
+  };
 
   return (
     <div>
       <Header />
-      <Hero onSignUp={'registmodal'} onSignIn={() => setShow(true)} />
+      <Hero onSignUp={() => setOpenRgs(true)} onSignIn={() => setOpenLgn(true)} />
 
-      <Login show={show} onClose={() => setShow(false)} onSubmit={'submit'} onDirect={'open modal regist'}>
+      <Register show={openRgs} onClose={() => setOpenRgs(false)} onSubmit={handleRegister} onDirect={openAnotherModal}>
+        <Input type="email" placeholder="Email" name="email" value={email} onChange={handleChange} />
+        <Input type="password" placeholder="Password" name="password" value={password} onChange={handleChange} />
+        <Input type="text" placeholder="Full name" name="fullname" value={fullname} onChange={handleChange} />
+        <Select onChange={handleChange} name="gender">
+          <option value="DEFAULT" disabled className="bg-primary">
+            Gender
+          </option>
+          <Option value="Male" field="Male" />
+          <Option value="Female" field="Female" />
+        </Select>
+        <Input type="number" placeholder="Phone" name="phone" value={phone} onChange={handleChange} />
+        <Input type="text" placeholder="Address" name="address" value={address} onChange={handleChange} />
+      </Register>
+
+      <Login show={openLgn} onClose={() => setOpenLgn(false)} onSubmit={handleLogin} onDirect={openAnotherModal}>
         <Input type="email" placeholder="Email" name="email" value={email} onChange={handleChange} />
         <Input type="password" placeholder="Password" name="password" value={password} onChange={handleChange} />
       </Login>
