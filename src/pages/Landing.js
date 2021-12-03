00268b54f94f17dfdca583a4703e1b578725a41b
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-12-03 12:43:59 +0700

    practice formik registration

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,14 +2,17 @@ import { useState, useContext, useEffect } from 'react';
 import { API, setAuthToken } from '../config/api';
 import { AuthContext } from '../context/authContext';
 import { useHistory } from 'react-router';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
-import Header from '../components/organism/Header';
-import Hero from '../components/organism/Hero';
-import Login from './auth/Login';
-import Register from './auth/Register';
-import Alert from '../components/atoms/Alert';
 import { Input, Option, Select } from '../components/atoms/Form';
 import { LoginMsg } from '../components/atoms/Message';
+import Alert from '../components/atoms/Alert';
+import Header from '../components/organism/Header';
+import Hero from '../components/organism/Hero';
+
+import Login from './auth/Login';
+import Register from './auth/Register';
 
 export default function Home() {
   const history = useHistory();
@@ -30,6 +33,14 @@ export default function Home() {
 
   const { email, password, fullname, phone, address } = form;
 
+  // HANDLE FORM INPUT WITH FORMIK
+  const SignupSchema = Yup.object().shape({
+    fullname: Yup.string().min(2, 'Fullname must be 2 characters or more!').max(20, 'Too Long!').required('Fullname cannot be empty'),
+    password: Yup.string().min(6, 'Password must be 6 characters or more!').max(20, 'Too Long!').required('Password cannot be empty'),
+    email: Yup.string().email('Invalid email').required('Email cannot be empty'),
+  });
+
+  // function to monitor every changes char in form input to form state
   const handleChange = (e) => {
     setForm({
       ...form,
@@ -166,12 +177,47 @@ export default function Home() {
     return () => ac.abort();
   }, []);
   return (
-    <div className="relative">
+    <div className="relative bg-primary">
       {message && message}
       <Header />
       <Hero onSignUp={() => setOpenRgs(true)} onSignIn={() => setOpenLgn(true)} />
 
-      <Register show={openRgs} onClose={() => setOpenRgs(false)} onSubmit={handleRegister} onDirect={openAnotherModal}>
+      {/* with formik register */}
+      <div className="pb-10 text-white">
+        <Formik
+          validateOnChange
+          initialValues={{
+            fullname: '',
+            password: '',
+            email: '',
+          }}
+          validationSchema={SignupSchema}
+          onSubmit={(values) => {
+            // same shape as initial values
+            console.log(values);
+          }}
+        >
+          {({ errors, touched }) => (
+            <Form className="w-full flex-col justify-center items-center p-6 border-2 space-y-5 border-gray-300">
+              <div className="relative">
+                <Field className="bg-primary border border-gray-400 w-full" name="fullname" placeholder="input" />
+                {errors.fullname && touched.fullname ? <div className="absolute top-6 text-red-400 text-xs mt-1">{errors.fullname}</div> : null}
+              </div>
+              <div className="relative">
+                <Field className="bg-primary border border-gray-400 w-full" name="password" type="password" placeholder="input" />
+                {errors.password && touched.password ? <div className="absolute top-6 text-red-400 text-xs mt-1">{errors.password}</div> : null}
+              </div>
+              <div className="relative">
+                <Field className="bg-primary border border-gray-400 w-full" name="email" type="email" placeholder="input" />
+                {errors.email && touched.email ? <div className="absolute top-6 text-red-400 text-xs mt-1">{errors.email}</div> : null}
+              </div>
+              <button type="submit">Submit</button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+
+      {/* <Register show={openRgs} onClose={() => setOpenRgs(false)} onSubmit={handleRegister} onDirect={openAnotherModal}>
         <Input type="email" placeholder="Email" name="email" value={email} onChange={handleChange} />
         <Input type="password" placeholder="Password" name="password" value={password} onChange={handleChange} />
         <Input type="text" placeholder="Full name" name="fullname" value={fullname} onChange={handleChange} />
@@ -184,7 +230,7 @@ export default function Home() {
         </Select>
         <Input type="number" placeholder="Phone" name="phone" value={phone} onChange={handleChange} />
         <Input type="text" placeholder="Address" name="address" value={address} onChange={handleChange} />
-      </Register>
+      </Register> */}
 
       <Login show={openLgn} onClose={() => setOpenLgn(false)} onSubmit={handleLogin} onDirect={openAnotherModal}>
         <Input type="email" placeholder="Email" name="email" value={email} onChange={handleChange} />
