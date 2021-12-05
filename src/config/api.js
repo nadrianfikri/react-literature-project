7fae51e1c262751bfb3bc31048da7e39d27fb270
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-12-05 20:00:43 +0700

    add env base api url for production

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // create base url
 export const API = axios.create({
-  baseURL: 'http://localhost:9000/api/v1',
+  baseURL: process.env.REACT_APP_BASE_API_URL || 'https://literature-api-nadrian.herokuapp.com/api/v1' || 'http://localhost:9000/api/v1',
 });
 
 // set auth token header
