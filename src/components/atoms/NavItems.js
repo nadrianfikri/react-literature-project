451b4e0bddfcd4d24a0508edc0373bc23291179a
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-12-07 23:17:29 +0700

    change notification admin

--- src/components/atoms/NavItems.js
+++ src/components/atoms/NavItems.js
@@ -11,7 +11,7 @@ function NavItem(props) {
   return (
     <NavLink activeClassName="text-red-600" to={props.to} className={`group flex flex-col items-center text-gray-200 text-lg hover:text-red-600 ${props.className}`}>
       {props.text}
-      <NavLink to={props.to} activeClassName="w-1/2 bg-danger" style={{ height: 2 }} className="w-0  group-hover:bg-danger group-hover:w-1/2 transition-all duration-500 "></NavLink>
+      <NavLink to={props.to} activeClassName="md:w-1/2 bg-danger" style={{ height: 2 }} className="w-0  group-hover:bg-danger md:group-hover:w-1/2 transition-all duration-500 "></NavLink>
     </NavLink>
   );
 }
